a319a413a4c9a488af98b92c50d0fc075abb260e
Amanda Pettenati <amanda@pette.net>
2021-11-22 09:43:33 -0500

    Create MUI theme and increase font size

---
+++ src/assets/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@material-ui/core/styles";
+
+// import { green, purple } from "@material-ui/core/colors";
+
+export const theme = createTheme({
+  typography: {
+    fontSize: 16,
+  },
+  // palette: {
+  //   primary: {
+  //     main: green[300],
+  //   },
+  //   secondary: {
+  //     main: purple[300],
+  //   },
+  // },
+});
